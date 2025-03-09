7583bb49e5a8b0d18e4d41b45d0d77faedb3ef79
singhAvneet <asingh@gms.ca>
2025-03-08 21:57:26 -0600

    Refactor web sockets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,47 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Injectable, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WebSocketSubject } from 'rxjs/webSocket';
-
+import { CommonModule } from '@angular/common'; // Import this
+export interface Message {
+  phone: number;
+  count: number;
+  ttl: any;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ CommonModule],
 })
 export class AppComponent implements OnInit, OnDestroy {
   serviceStatus: any;
+   numberCount: Map<number, number> = new Map();
+
+  ttl:any;
   private socket!: WebSocketSubject<any>;
   private wsUrl = 'ws://localhost:5011/ws';
-  // private wsUrl = 'ws://localhost:5000/ws';
-  ngOnInit() {
+
+  ngOnInit() {    
     this.socket = new WebSocketSubject(this.wsUrl);
     this.socket.subscribe(
-      (data: any) => {
+      (data: Message) => {
+        if(data.count!=0)      
+      this.numberCount.set(data.phone,data.count);
+      this.ttl = data.ttl
         this.serviceStatus = {
-          name: 'My .NET Service',
-          online: data.online,
-          lastChecked: data.timestamp
-        };
+          phone: data.phone,
+          count: data.count,
+          ttl: data.ttl,
+                };
       },
       error => {
         console.error('WebSocket error:', error);
       }
     );
   }
-
+  get mapEntries() {
+    return Array.from(this.numberCount.entries());
+  }
   ngOnDestroy() {
     if (this.socket) {
       this.socket.unsubscribe();
