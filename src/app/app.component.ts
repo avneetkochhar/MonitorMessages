9b9b0342c04a8d06d5b9a64be26ced5f0e3fabad
singhAvneet <asingh@gms.ca>
2025-03-09 21:36:37 -0600

    Refactor and tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,41 @@
-import { Component, Injectable, OnDestroy, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WebSocketSubject } from 'rxjs/webSocket';
 import { CommonModule } from '@angular/common'; // Import this
-export interface Message {
-  phone: number;
-  count: number;
-  ttl: any;
-}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [ CommonModule],
+  imports: [CommonModule],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  serviceStatus: any;
-   numberCount: Map<number, number> = new Map();
 
-  ttl:any;
+  accountStatus: Map<string, Object> = new Map();
+
   private socket!: WebSocketSubject<any>;
+
   private wsUrl = 'ws://localhost:5011/ws';
 
-  ngOnInit() {    
+  ngOnInit() {
+
     this.socket = new WebSocketSubject(this.wsUrl);
+
     this.socket.subscribe(
-      (data: Message) => {
-        if(data.count!=0)      
-      this.numberCount.set(data.phone,data.count);
-      this.ttl = data.ttl
-        this.serviceStatus = {
-          phone: data.phone,
-          count: data.count,
-          ttl: data.ttl,
-                };
+      (data: any) => {
+        if (data != null) {
+
+          let account = {
+            accountId: data.accountID,
+            accountLimit: data.accountLimit
+          }
+          let status = {
+            account: account,
+            businessPhone: data.array
+          }
+
+          this.accountStatus.set(data.accountID, status);
+
+        }
       },
       error => {
         console.error('WebSocket error:', error);
@@ -40,8 +43,9 @@ export class AppComponent implements OnInit, OnDestroy {
     );
   }
   get mapEntries() {
-    return Array.from(this.numberCount.entries());
+    return Array.from(this.accountStatus.entries());
   }
+  
   ngOnDestroy() {
     if (this.socket) {
       this.socket.unsubscribe();
